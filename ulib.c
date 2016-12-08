1941e7df43e169d36bb79d69b812befcb7234088
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2016-12-08 11:25:16 -0500

    Damn signal

--- ulib.c
+++ ulib.c
@@ -107,7 +107,7 @@ memmove(void *vdst, void *vsrc, int n)
 //need to change the restorer
 void restorer(void);
 __asm__ ("restorer:\n\t"
-            "addl $0x4,%esp\n\t"
+            "addl $0xc,%esp\n\t"
             "pop %edx\n\t"
             "pop %ecx\n\t"
             "pop %eax\n\t"
